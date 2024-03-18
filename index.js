2649f9aeaac4e3d4a5214905bdf47863bbe96dd9
Abdullahmohsen627 <155685835+Abdullahmohsen627@users.noreply.github.com>
2024-03-18 11:50:19 -0400

    nn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const port = process.env.PORT || 4000;
 const app = express();
 
-app.get('/', (req, res) => res.send('<h1>hellow kind man THANKS!</h1>'));
+app.get('/', (req, res) => res.send('<h1>hellow kind man THANKSss!</h1>'));
 
 app.listen(port, () => {
     console.log(`app is up and running on port: ${port}`);
